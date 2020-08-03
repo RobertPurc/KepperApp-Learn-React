40c2ef94d9da00300e332439c3e179b37679fafe
RobertPurc <robert.purc12@gmail.com>
2020-08-03 11:50:16 +0200

    Setup of app

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import ReactDOM from "react-dom";
+import App from "./componenets/App";
+// import styles from '../public/s'
+
+ReactDOM.render(<App />, document.getElementById("root"));
+//6. Make sure that the final website is styled like the example shown here:
+//https://l1pp6.csb.app/
+
+//HINT: You will need to study the classes in teh styles.css file to appy styling.
